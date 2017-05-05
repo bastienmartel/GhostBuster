dd2be0aa924fae740d1a5c602018751478f8b192
gprog39 <pyfes@aol.com>
2017-05-05 16:19:48 +0200

    ajout des classes: Partie, Joueur, Case, Plateau

---
+++ WindowsFormsApplication2/Partie.cs
@@ -0,0 +1,17 @@
+﻿namespace WindowsFormsApplication2
+{
+    public class Partie
+    {
+        private int nbMinesRestantes;
+        private int nbMinesInitial;
+        private bool enCours; //Indique si la partie est en cours d'exécution
+
+        public Partie(int nbMinesRestantes, int nbMinesInitial, bool enCours)
+        {
+            this.nbMinesRestantes = nbMinesRestantes;
+            this.nbMinesInitial = nbMinesInitial;
+            this.enCours = enCours;
+        }
+
+    }
+}
